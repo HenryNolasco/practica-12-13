a339f0b591df9f093f6f13665d74e5791364db93
Henry Nolasco <nlascoh.00@gmail.com>
2025-06-11 22:38:26 -0600

    make branch development

---
+++ students.service.js
@@ -0,0 +1,15 @@
+
+
+const makeStudentsService = (name, lastname, age) => {
+    
+    try {
+        return `your name:${name}\nyour lastname : ${lastname}\n your age : ${age}`
+    } catch (error) {
+        return error
+    }
+
+}
+
+const makeStudent = makeStudentsService("Henry", "Nolasco", 23)
+
+console.log(makeStudent);
